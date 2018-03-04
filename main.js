7a6c5824b5e287f3934605c69c293df994770771
Amanda Slayton <alslayton86@gmail.com>
2018-03-04 16:03:12 -0600

    challenge 2 complete

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 console.log("Why, hello there.");
 function writeToDOM(domString,domId){
   var myBody = document.getElementById("writeHere");
-  myBody.innerHTML = "<div id='" + domId + "'></div>";
+  myBody.innerHTML += "<div id='" + domId + "'></div>";
   var myElement = document.getElementById(domId);
   myElement.innerHTML += "<h1>" + domId + "</h1>";
   myElement.innerHTML += "<h3>" + domString + "</h3>";
@@ -25,3 +25,37 @@ function ageThing(birthYear, myYear){
 };
 
 writeToDOM(ageThing(1986,1956),"challenge-1");
+
+//Challenge 2
+
+function tryMe(myNum){
+  var resultArray = [];
+  for(var i = 2; i < myNum; i++){
+    if(myNum % i === 0){
+      var isPrime = false;
+    }else{
+      var isPrime = true;
+    };
+  };
+  resultArray.push(isPrime);
+  
+  if(myNum % 2 === 0){
+    var isEven = true;
+  }else{
+    var isEven = false;
+  };
+  
+  resultArray.push(isEven);
+
+  if(myNum % 10 === 0){
+    var multOf10 = true;
+  }else{
+    var multOf10 = false;
+  };
+  resultArray.push(multOf10);
+
+  return resultArray;
+};
+
+writeToDOM(tryMe(12),"challenge-2");
+
